b567cf0021ddc0b1247f39511066651210c88447
Oksovskii <32220874+Oksovskii@users.noreply.github.com>
2021-04-10 21:52:50 +0300

    Update manga_load.py

--- manga_load.py
+++ manga_load.py
@@ -3,13 +3,21 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
 import time
+from progress.bar import IncrementalBar
+import warnings
+
+warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
 
 def parse_links():
+    print("Инициализация вебдрайвера...")
     driver = webdriver.Firefox()
-    driver.get("https://jaomix.ru/category/dikaya-gotovka/") #link on manga/ranobe on jaomix.ru for example "https://jaomix.ru/lenivyj-mechnik/""
+    print("Вебдрайвер инициализирован")
+    driver.get(input('Введите ссылку для скачивания с "jaomix.ru": ')) #link on manga/ranobe on jaomix.ru for example "https://jaomix.ru/lenivyj-mechnik/""
+    print("Получение ссылок глав...")
     try:
         data = ''
         select = Select(driver.find_element_by_xpath("(//select[@class='sel-toc new-ajax-load'])"))
+        print(select.options)
         for element in select.options:
             element.click()
             time.sleep(0.1) #Delay for safety
@@ -26,18 +34,24 @@ def parse_links():
     except:
         visibletab = driver.find_element_by_xpath('//div[@class="hiddenstab active"]')
 
-        soup = BeautifulSoup(visibletab.get_attribute('innerHTML'))
+        soup = BeautifulSoup(visibletab.get_attribute('innerHTML'),"html.parser")
         for a in soup.find_all('a', href=True):
             data+=str('https://jaomix.ru'+a['href']+'\n')
 
         links = open("links.txt","w",encoding='utf8') #File with links
         links.write(data)
         links.close()
+        print("Ссылки получены")
 
 def download_from_links():
     textfile = open("links.txt") #Import file with links
     lines = textfile.readlines()
     tex = ''
+
+    with open('links.txt') as f:
+        size=len([0 for _ in f])
+    print("Запись глав")
+    bar = IncrementalBar('Глав записано:', max = size)
 
     for line in reversed(lines):
         text = open("text.txt","a",encoding='utf8') #File with ready text
@@ -47,7 +61,7 @@ def download_from_links():
 
         data = r.text
         data1 = data.encode('UTF-8')
-        soup = BeautifulSoup(data1)
+        soup = BeautifulSoup(data1,"html.parser")
 
         st1=soup.find("h1", {'class': 'entry-title'}) #Номер главы
         st2=soup.find("div", {'class': 'entry-content'}) #Содержание
@@ -57,6 +71,9 @@ def download_from_links():
 
         text.write(correcting+'\n')
         text.close()
+        bar.next()
+    bar.finish()
+    print("Главы записаны и доступны в файле 'text.txt'")
 
 parse_links()
 download_from_links()
